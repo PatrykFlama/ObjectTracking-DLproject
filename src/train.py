ae61bf24166e6135a76a4fc7ce887109fec1711b
PatrykFlama <patrykflama@gmail.com>
2026-03-25 13:46:01 +0100

    Fix absolute path

--- src/train.py
+++ src/train.py
@@ -1,9 +1,14 @@
+from pathlib import Path
+
 from RF_DETR import RFDETRT
 
 if __name__ == "__main__":
+    project_root = Path(__file__).resolve().parents[1]
+    dataset_dir = project_root / "dataset"
+
     rfdetr = RFDETRT("nano")
     rfdetr.train(
-        "/Users/piotrek/ObjectTracking-DLproject/dataset",
+        str(dataset_dir),
         epochs=10,
         batch_size=4,
     )
