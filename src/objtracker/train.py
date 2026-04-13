85f10c34aec43ba31a7159a970084f3c3fdb2256
bialykaloryfer <piotr.salamon03@gmail.com>
2026-04-13 21:35:37 +0200

    ty

--- src/objtracker/train.py
+++ src/objtracker/train.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-from objtracker.RF_DETR import RFDETRT
+from objtracker.RF_DETR import RFDETRTrainer
 
 if __name__ == "__main__":
     project_root = Path(__file__).resolve().parents[1]
     dataset_dir = project_root / "dataset"
 
-    rfdetr = RFDETRT("nano")
+    rfdetr = RFDETRTrainer("nano")
     rfdetr.train(
         str(dataset_dir),
         epochs=10,
