8a69c9fb56997092dddb5df69edcc1645521842c
PatrykFlama <patrykflama@gmail.com>
2026-04-18 14:37:13 +0200

    Add w&b

--- src/objtracker/train.py
+++ src/objtracker/train.py
@@ -1,18 +1,30 @@
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
 
-# 1. Import the new CocoDataModule instead of MOT15DataModule
 from objtracker.datasets.coco import CocoDataModule
 from objtracker.models.rf_detr import RFDETRLightning
 
 if __name__ == "__main__":
     print("Initializing Data Pipeline...")
-    # 2. Use the new module!
     data_module = CocoDataModule(batch_size=4)
 
     print("Initializing Model...")
     model = RFDETRLightning(model_size="nano", lr=1e-4)
 
+    print("Initializing Weights & Biases...")
+    # Setup the WandB Logger
+    wandb_logger = WandbLogger(
+        project="MOT15-Tracking",  # Name of the project in your WandB dashboard
+        name="RFDETR-nano-baseline",  # Name of this specific run
+        log_model="all",  # Automatically saves your best model weights to the cloud!
+    )
+
     print("Starting Lightning Trainer...")
-    trainer = pl.Trainer(max_epochs=10, accelerator="auto", log_every_n_steps=1)
+    trainer = pl.Trainer(
+        max_epochs=10,
+        accelerator="auto",
+        log_every_n_steps=1,
+        logger=wandb_logger,  # Hand the logger to the Trainer
+    )
 
     trainer.fit(model, datamodule=data_module)
